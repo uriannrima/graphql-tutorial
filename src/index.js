7c4af50d01366b5a7b73ba7d897e8a1b6b0f9eac
Luciano Lima <luciano.lima@ingresso.com>
2018-10-05 00:15:00 -0300

    Authentication working. Migrate to apollo.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,18 @@
 /* eslint-disable no-console */
 const logger = require('./logger');
 const app = require('./app');
-const seeder = require('feathers-seeder');
-const seederOptions = require('./seeder-config');
+// const seeder = require('feathers-seeder');
+// const seederOptions = require('./seeder-config');
 
-app.configure(seeder(seederOptions));
+// app.configure(seeder(seederOptions));
 
-app.seed().then(() => {
-  const port = app.get('port');
-  const server = app.listen(port);
+const port = app.get('port');
+const server = app.listen(port);
 
-  process.on('unhandledRejection', (reason, p) =>
-    logger.error('Unhandled Rejection at: Promise ', p, reason)
-  );
+process.on('unhandledRejection', (reason, p) =>
+  logger.error('Unhandled Rejection at: Promise ', p, reason)
+);
 
-  server.on('listening', () =>
-    logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
-  );
-});
-
+server.on('listening', () =>
+  logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
+);
--- src/services/graphql/graphql.service.js
+++ src/services/graphql/graphql.service.js
@@ -1,8 +1,9 @@
 // Initializes the `graphql` service on path `/graphql`
 import { graphqlExpress, graphiqlExpress } from 'graphql-server-express';
 import { makeExecutableSchema } from 'graphql-tools';
+const { authenticate } = require('@feathersjs/authentication').express;
 // const createService = require('./graphql.class.js');
-const hooks = require('./graphql.hooks');
+// const hooks = require('./graphql.hooks');
 
 import Resolvers from './resolvers';
 import Schema from './schema';
@@ -20,15 +21,21 @@ module.exports = function (app) {
   //   paginate
   // };
 
+  const graphQlAuth = async (req, res, next) => {
+    // const { headers: { authorization } } = req.feathers;
+    // if (authorization) {
+    //   authenticate('jwt')(req, res, next);
+    // } else {
+    //   next();
+    // }
+    next();
+  };
+
   // Initialize our service with any options it requires
-  app.use('/graphql', graphqlExpress(req => {
-    let { headers: { authorization }, provider } = req.feathers;
+  app.use('/graphql', graphQlAuth, graphqlExpress(req => {
     return {
       schema: executableSchema,
-      context: {
-        token: authorization,
-        provider
-      }
+      context: req.feathers
     };
   }));
 
